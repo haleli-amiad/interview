4014142b4a4518f3fa48ec74718252fd1a85148f
haleli amiad <haleli.am@gmail.com>
2021-04-03 18:03:06 +0200

    initializing react app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './assets/styles/styles.scss';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+	return (
+		<main className="home main-layout">
+			<h1>BLA</h1>
+		</main>
+	);
+}
+
+export default Home;
